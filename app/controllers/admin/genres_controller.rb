92d3c5fe75bf1677986f15a6003d72a6e5eeeee4
EC2 Default User <ec2-user@ip-172-31-2-162.ap-northeast-1.compute.internal>
2022-08-18 08:13:50 +0000

    cheng jenre

--- app/controllers/admin/genres_controller.rb
+++ app/controllers/admin/genres_controller.rb
@@ -1,7 +1,26 @@
+# frozen_string_literal: true
 class Admin::GenresController < ApplicationController
+  # before_action :authenticate_user!#ログインしていない場合ログインに戻る指定
+  
   def index
+    @genre = Genre.new
+    @genres = Genre.all
   end
-
+  
   def edit
+    @genre = Genre.find(params[:id])
+  end
+  
+  # def edit
+  #   @book = Book.find(params[:id])
+  #   unless current_user == @book.user
+  #     redirect_to books_path
+  #   end
+  # end
+
+  private
+  
+  def genre_params
+    params.require(:genre).permit(:name)#dbマイグレートで確認
   end
 end
