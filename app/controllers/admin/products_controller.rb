e056efcc1fa817e543fa65b2c389bf38ec994c03
EC2 Default User <ec2-user@ip-172-31-2-162.ap-northeast-1.compute.internal>
2022-08-19 11:54:30 +0000

    add jenre product

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -1,13 +1,45 @@
 class Admin::ProductsController < ApplicationController
-  def new
-  end
-
+   
   def index
+    @products = Product.all
   end
 
+  def new
+    @product = Product.new
+  end
+  
+  # 不安箇所コメントアウト
+  # def add_tax_sales_price
+  # (self.sales_price * 1.10).round
+  # end
+  
   def show
+    @product = Product.find(params[:id])
   end
 
   def edit
+    @product = Product.find(params[:id])
+  end
+  
+  def create
+      @product = Product.new(product_params)
+    if @product.save
+       redirect_to admin_products_path(@product.id)#notice: 'You have created book successfully.'?
+    else
+      render:new
+    end
+  end
+  
+
+ 
+
+  def update
+    @product = Product.find(params[:id])
+   
+  end
+ private
+
+  def product_params
+    params.require(:product).permit(:genre_id, :name, :explanation, :non_taced_price, :image, :is_active)
   end
 end
