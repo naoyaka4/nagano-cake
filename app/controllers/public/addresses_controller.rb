3d447d12756f00cc7a53488f0420036342e9852b
Sugar56 <108616992+Sugar56@users.noreply.github.com>
2022-08-20 12:45:59 +0000

    配送先登録・一覧,配送先編集,マイページ,退会画面

--- app/controllers/public/addresses_controller.rb
+++ app/controllers/public/addresses_controller.rb
@@ -1,7 +1,42 @@
 class Public::AddressesController < ApplicationController
   def index
+    @address = Address.new
+    @addresses = Address.all
+  end
+
+  def create
+    @address = Address.new(address_params)
+
+    if @address.save
+      redirect_to edit_address_path(@address), notice: "You have created book successfully."
+    else
+      @address = Address.all
+      render 'index'
+    end
   end
 
   def edit
+    @address = Address.find(params[:id])
+  end
+
+  def update
+    @address = Address.find(params[:id])
+    if @address.update(address_params)
+      redirect_to addresses_path, notice: "You have updated book successfully."
+    else
+      render "edit"
+    end
+  end
+
+  def destroy
+    @address = Address.find(params[:id])
+    @address.destroy
+    redirect_to addresses_path
+  end
+
+  private
+
+  def address_params
+    params.require(:address).permit(:name, :address, :post_code)
   end
 end
