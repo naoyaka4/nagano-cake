194a52a6180f8912f6efe071c83053003c8a8dfb
keishimushup <108566690+keishimushup@users.noreply.github.com>
2022-08-22 04:04:49 +0000

    update model routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  
+
   devise_for :admin, skip: [:registrations, :passwords], controllers: {
   sessions: "admin/sessions"
 }
@@ -8,7 +8,7 @@ Rails.application.routes.draw do
   registrations: "public/registrations",
   sessions: 'public/sessions'
 }
-  
+
   namespace :admin do
     root to: "homes#top"
     resources :orders, only: [:show, :update]
@@ -17,15 +17,22 @@ Rails.application.routes.draw do
     resources :products, only: [:new, :index, :show, :edit, :create, :update, :destroy]
     patch 'order_details/:id' => 'order_details#update'
   end
-  
-  
+
+
   scope module: :public do
     root to: 'homes#top'
     get '/about' => 'homes#about'
+    get '/customers/check' => 'customers#check'
+    patch '/customers/withdraw' => 'customers#withdraw'
+    get '/customers/my_page' => 'customers#show'
+    get '/customer/information/edit' => 'customers#edit'
+    patch '/customer/information' => 'customers#update'
     resources :addresses, only: [:index, :edit, :create, :update, :destroy]
-    resources :orders, only: [:new, :index, :show, :check, :complete, :create]
-    resources :cart_products, only: [:index, :update, :destroy, :destroy_all, :create]
-    resources :customers, only: [:show, :edit, :check, :withdraw, :update]
+    resources :orders, only: [:new, :index, :show, :create]
+    post '/orders/check' => 'orders#check'
+    get '/orders/complete' => 'orders#complete'
+    resources :cart_products, only: [:index, :update, :destroy, :create]
+    delete '/cart_products/destroy_all' => 'cart_products#destroy_all'
     resources :products, only: [:index, :show]
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
