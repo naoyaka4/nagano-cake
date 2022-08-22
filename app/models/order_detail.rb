ff02cb3c134204dc6183dab7b587f4b7028990fa
keishimushup <108566690+keishimushup@users.noreply.github.com>
2022-08-22 13:30:29 +0000

    update 注文履歴詳細

--- app/models/order_detail.rb
+++ app/models/order_detail.rb
@@ -1,4 +1,6 @@
 class OrderDetail < ApplicationRecord
+  enum making_status: {production_impossible: 0, awaiting_production: 1, in_production: 2, production_completed: 3 }
+  
   belongs_to :product
   belongs_to :order
 end
