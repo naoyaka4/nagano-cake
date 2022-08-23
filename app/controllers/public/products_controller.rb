069fd3c179a07da83243434b6d9047964ca7c397
Sugar56 <108616992+Sugar56@users.noreply.github.com>
2022-08-23 07:30:38 +0000

    [商品一覧,詳細記述]

--- app/controllers/public/products_controller.rb
+++ app/controllers/public/products_controller.rb
@@ -1,7 +1,11 @@
 class Public::ProductsController < ApplicationController
   def index
+    @genres = Genre.all
+    @products = Product.page(params[:page])
   end
 
   def show
+    @product = Product.find(params[:id])
+    @cart_product = current.customer
   end
 end
