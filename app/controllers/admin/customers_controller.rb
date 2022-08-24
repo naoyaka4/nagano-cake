d27b8469697eec7b7dc5c388a6ce58eb821a7bdf
keishimushup <108566690+keishimushup@users.noreply.github.com>
2022-08-24 09:46:33 +0000

    add 注文完了画面　注文履歴詳細画面

--- app/controllers/admin/customers_controller.rb
+++ app/controllers/admin/customers_controller.rb
@@ -10,20 +10,20 @@ class Admin::CustomersController < ApplicationController
   def edit
     @customer = Customer.find(params[:id])
   end
-  
+
   def update
     @customer = Customer.find(params[:id])
     if @customer.update(customer_params)
-      redirect_to admin_customer_path(@customer), notice: "You have updated customer successfully."
+      redirect_to admin_customer_path(@customer), notice: "会員情報を編集しました"
     else
       render "edit"
     end
   end
-  
+
   private
-  
+
   def customer_params
     params.require(:customer).permit(:last_name, :first_name, :kana_last_name, :kana_first_name, :post_code, :address, :phone_number, :email, :is_deleted)
   end
-  
+
 end
