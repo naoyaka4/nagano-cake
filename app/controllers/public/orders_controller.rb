6a2a57beaded616a8606a9ca098e82cb1d5f2aa6
EC2 Default User <ec2-user@ip-172-31-2-162.ap-northeast-1.compute.internal>
2022-08-24 07:49:56 +0000

    add customer

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -12,9 +12,48 @@ class Public::OrdersController < ApplicationController
   end
 
   def check
+    @order = Order.new(order_params)
+    if params[:order][:a] == '0'
+       @order.post_code = current_customer.post_code
+       @order.address = current_customer.address
+       @order.name = current_customer.first_name + current_customer.last_name
+    elsif params[:order][:a] == '1'
+          @address = Address.find(params[:order][:address_id])
+    elsif params[:order][:a] == '2'
+          @order.post_code = @address.post_code
+          @order.address = @address.address
+          @order.name = current_customer.first_name + current_customer.last_name
+      
+    end
+    # @address = Address.find(params[:order_id][:address_id])？
+    @order.cost = 800
+    @total_payment = 0
+    @cart_products = CartProduct.all
     
+    #こっち？ @cart_products = CartProduct.all
+
+  
   end
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
 
   def complete
+  end
+
+
+  private
+
+  def order_params
+    params.require(:order).permit(:customer_id, :cost, :status, :payment_method, 
+    :name, :post_code, :address, :total_payment, :created_at, :updated_at)
   end
 end
