749db5bf2d6b1442413c1164516dc58ff3a6dca0
keishimushup <108566690+keishimushup@users.noreply.github.com>
2022-08-24 08:56:16 +0000

    add 会員編集

--- app/controllers/public/customers_controller.rb
+++ app/controllers/public/customers_controller.rb
@@ -4,6 +4,16 @@ class Public::CustomersController < ApplicationController
   end
 
   def edit
+    @customer = current_customer
+  end
+
+  def update
+    @customer = current_customer
+    if @customer.update(customer_params)
+      redirect_to customers_my_page_path, notice: "会員情報を編集しました"
+    else
+      render "edit"
+    end
   end
 
   def check
@@ -16,4 +26,12 @@ class Public::CustomersController < ApplicationController
     flash[:notice] = "退会処理を実行致しました"
     redirect_to root_path
   end
+
+
+   private
+
+  def customer_params
+    params.require(:customer).permit(:last_name, :first_name, :kana_last_name, :kana_first_name, :post_code, :address, :phone_number, :email, :is_deleted)
+  end
+
 end
