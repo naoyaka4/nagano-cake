59890964b6b0a1b9ef96ed6335f0d554c05f07e1
Sugar56 <108616992+Sugar56@users.noreply.github.com>
2022-08-26 08:09:35 +0000

    [画像表示追加]

--- app/controllers/public/products_controller.rb
+++ app/controllers/public/products_controller.rb
@@ -4,14 +4,14 @@ class Public::ProductsController < ApplicationController
     @products = Product.page(params[:page])
     @products1 = Product.all
   end
-  
+
 
 
   def show
     @genres = Genre.all
     @product = Product.find(params[:id])
     @cart_product = CartProduct.new
-    
+
     # @products = current_customer.products.new(product_params)
     # @products.save
     # redirect_to product_path(@cart_product.product_id)
