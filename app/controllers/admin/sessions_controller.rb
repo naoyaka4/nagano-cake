19cea7c60d2c52c0a07c849d69af7056e2c2ba38
Sugar56 <108616992+Sugar56@users.noreply.github.com>
2022-08-27 04:06:30 +0000

    [admin側ログアウト後遷移先変更]

--- app/controllers/admin/sessions_controller.rb
+++ app/controllers/admin/sessions_controller.rb
@@ -27,5 +27,9 @@ class Admin::SessionsController < Devise::SessionsController
   def after_sign_in_path_for(resource)
    admin_root_path #routes passを使う
   end
-  
+
+  def after_sign_out_path_for(resource)
+    new_admin_session_path
+  end
+
 end
